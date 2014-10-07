11d64744a5cda39971525647f32195cab4d36a56
Viraj Chitnis <chitnisviraj@gmail.com>
2014-10-07 14:36:19 -0400

    Connections are killed after a response is sent
    Once the server has sent a response to the client, the connection and in turn its thread is killed.

--- WebServer.java
+++ WebServer.java
@@ -39,16 +39,11 @@ class WebServer {
 		 */
 		try {
 			ServerSocket welcomeSocket = new ServerSocket(9999);
-			ArrayList<ServerClientThread> serverThreads = new ArrayList<ServerClientThread>();
-			
-			Timer garbageCollectionTimer = new Timer();
-			garbageCollectionTimer.scheduleAtFixedRate(new GarbageCollector(serverThreads), 500, 500);
 			
 			while(true) {
 				Socket connectionSocket = welcomeSocket.accept();
 				ServerClientThread newRunnable = new ServerClientThread(connectionSocket);
 				new Thread(newRunnable).start();
-				serverThreads.add(newRunnable);
 			}
 		}
 		catch (IOException e) {
@@ -100,125 +95,89 @@ class WebServer {
 	}
 }
 
-class GarbageCollector extends TimerTask {
-	ArrayList<ServerClientThread> serverThreads;
-	
-	public GarbageCollector(ArrayList<ServerClientThread> serverThreads) {
-		this.serverThreads = serverThreads;
-	}
-	
-	public void run() {
-		Date dateNow = new Date();
-		for (int i = 0; i < serverThreads.size(); i++) {
-			ServerClientThread currThread = serverThreads.get(i);
-			long timeSinceThreadStart = dateNow.getTime() - currThread.threadStartTime.getTime();
-			Boolean transferInProgress = currThread.dataTransferInProgress;
-			if ((timeSinceThreadStart > 300) && (!transferInProgress)) {
-				currThread.closeOpenConnections();
-				serverThreads.remove(currThread);
-			}
-		}
-	}
-}
-
 class ServerClientThread implements Runnable {
 	protected Socket connectionSocket;
 	protected String serverName;
 	protected BufferedReader inFromClient;
 	protected DataOutputStream outToClient;
-	public Date threadStartTime;
-	protected Boolean threadRunning;
-	public Boolean dataTransferInProgress;
 
     public ServerClientThread(Socket connectionSocket) {
         this.connectionSocket = connectionSocket;
 		this.serverName = "CHITNIS/1.0";
-		this.threadStartTime = new Date();
-		this.threadRunning = true;
-		dataTransferInProgress = false;
     }
 
     public void run() {
 		try {
 			inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 			outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-		
-			Boolean keepConnectionOpen = true;
-			while (keepConnectionOpen && threadRunning) {
-				ArrayList<String> httpRequestHeaders = new ArrayList<String>();
-				while (keepConnectionOpen && threadRunning) {
-					String tempClientText = inFromClient.readLine();
-					if (tempClientText == null || tempClientText.length() == 0) {
-						if (httpRequestHeaders.size() == 0) {
-							continue;
-						}
-						else {
-							break;
-						}
-					}
-					else {
-						dataTransferInProgress = true;
-						httpRequestHeaders.add(tempClientText);
-					}
-				}
 			
-				if (httpRequestHeaders.size() >= 2) {
-					String clientGETRequest = httpRequestHeaders.get(0);
-					if ((clientGETRequest.length() < 14) || (!clientGETRequest.startsWith("GET")) || 
-					((!clientGETRequest.endsWith("HTTP/1.1")) && (!clientGETRequest.endsWith("HTTP/1.0")))) {
-						keepConnectionOpen = returnBadRequest();
-						threadStartTime = new Date();
-						dataTransferInProgress = false;
+			ArrayList<String> httpRequestHeaders = new ArrayList<String>();
+			while (true) {
+				String tempClientText = inFromClient.readLine();
+				if (tempClientText == null || tempClientText.length() == 0) {
+					if (httpRequestHeaders.size() == 0) {
 						continue;
-					}
-				
-					String clientHOSTRequest = httpRequestHeaders.get(1);
-					if (!clientHOSTRequest.startsWith("Host: ")) {
-						keepConnectionOpen = returnBadRequest();
-						threadStartTime = new Date();
-						dataTransferInProgress = false;
-						continue;
-					}
-					
-					String clientURIRequest = clientGETRequest.substring(4, (clientGETRequest.length() - 9));
-					if (clientURIRequest.equals("/")) {
-						String clientIP = connectionSocket.getInetAddress().getHostAddress();
-						Date currentDate = new Date();
-	
-						String responseBody = "<html><head><title>Viraj's Web Server</title></head><body>"
-								+ "<p>Your IP address is " + clientIP + "<br>"
-								+ "It is now " + currentDate.toString() + "</p>"
-								+ "<br><p><b>Your request headers:</b><br>";
-						for (int i = 0; i < httpRequestHeaders.size(); i++) {
-							responseBody = responseBody + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + httpRequestHeaders.get(i) + "<br>";
-						}
-						responseBody = responseBody + "</p></body></html>";
-						int responseLength = (int)responseBody.length();
-	
-						String serverResponse = "HTTP/1.1 200 OK\r\n"
-							+ "Date: " + getServerHTTPTime() + "\r\n"
-							+ "Connection: close\r\n"
-							+ "Server: " + serverName + "\r\n"
-							+ "Content-Length: " + responseLength + "\r\n"
-							+ "Content-Type: text/html; charset=UTF-8\r\n\r\n\r\n"
-							+ responseBody + "\n";
-						keepConnectionOpen = writeBytesToClient(serverResponse);
-						threadStartTime = new Date();
-						dataTransferInProgress = false;
 					}
 					else {
-						keepConnectionOpen = returnNotFoundRequest();
-						threadStartTime = new Date();
-						dataTransferInProgress = false;
-						continue;
+						break;
 					}
 				}
 				else {
-					keepConnectionOpen = returnBadRequest();
-					threadStartTime = new Date();
-					dataTransferInProgress = false;
-					continue;
+					httpRequestHeaders.add(tempClientText);
 				}
+			}
+		
+			if (httpRequestHeaders.size() >= 2) {
+				String clientGETRequest = httpRequestHeaders.get(0);
+				if ((clientGETRequest.length() < 14) || (!clientGETRequest.startsWith("GET")) || 
+				((!clientGETRequest.endsWith("HTTP/1.1")) && (!clientGETRequest.endsWith("HTTP/1.0")))) {
+					returnBadRequest();
+					closeOpenConnections();
+					return;
+				}
+			
+				String clientHOSTRequest = httpRequestHeaders.get(1);
+				if (!clientHOSTRequest.startsWith("Host: ")) {
+					returnBadRequest();
+					closeOpenConnections();
+					return;
+				}
+				
+				String clientURIRequest = clientGETRequest.substring(4, (clientGETRequest.length() - 9));
+				if (clientURIRequest.equals("/")) {
+					String clientIP = connectionSocket.getInetAddress().getHostAddress();
+					Date currentDate = new Date();
+
+					String responseBody = "<html><head><title>Viraj's Web Server</title></head><body>"
+							+ "<p>Your IP address is " + clientIP + "<br>"
+							+ "It is now " + currentDate.toString() + "</p>"
+							+ "<br><p><b>Your request headers:</b><br>";
+					for (int i = 0; i < httpRequestHeaders.size(); i++) {
+						responseBody = responseBody + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + httpRequestHeaders.get(i) + "<br>";
+					}
+					responseBody = responseBody + "</p></body></html>";
+					int responseLength = (int)responseBody.length();
+
+					String serverResponse = "HTTP/1.1 200 OK\r\n"
+						+ "Date: " + getServerHTTPTime() + "\r\n"
+						+ "Connection: close\r\n"
+						+ "Server: " + serverName + "\r\n"
+						+ "Content-Length: " + responseLength + "\r\n"
+						+ "Content-Type: text/html; charset=UTF-8\r\n\r\n\r\n"
+						+ responseBody + "\n";
+					writeBytesToClient(serverResponse);
+					closeOpenConnections();
+				}
+				else {
+					returnNotFoundRequest();
+					closeOpenConnections();
+					return;
+				}
+			}
+			else {
+				returnBadRequest();
+				closeOpenConnections();
+				return;
 			}
 		}
 		catch (IOException e) {
@@ -277,7 +236,6 @@ class ServerClientThread implements Runnable {
 		try {
 			outToClient.flush();
 			connectionSocket.close();
-			threadRunning = false;
 		}
 		catch (IOException e) {
 			e.printStackTrace();
